b0fee30251cead6e5f9dcdad57e0832cd40c8950
Lukeylas <120398798+Lukeylas@users.noreply.github.com>
2024-10-13 17:24:41 +0200

    Initial commit

---
+++ app/Http/Controllers/ItemController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Item;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class ItemController extends Controller
+{
+    /**
+     * Display all possesed items.
+     */
+    public function index(): Response
+    {
+        return Inertia::render('Items/Index', [
+            //
+        ]);
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $validated = $request->validate(([
+            'name' => 'required|string|max:255'
+        ]));
+
+        $request->user()->items()->create($validated);
+
+        return redirect(route('items.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Item $item)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Item $item)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Item $item)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Item $item)
+    {
+        //
+    }
+}
---
+++ resources/js/Pages/Items/Index.vue
@@ -0,0 +1,29 @@
+<template>
+    <Head title="Items" />
+
+    <AuthenticatedLayout>
+        <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
+            <form @submit.prevent="form.post(route('items.store'), { onSuccess: () => form.reset() })">
+                <input
+                    type="text"
+                    v-model="form.name"
+                    placeholder="What new item did you hoard?"
+                    class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
+                ></input>
+                <InputError :message="form.errors.message" class="mt-2" />
+                <PrimaryButton class="mt-4">Add Item</PrimaryButton>
+            </form>
+        </div>
+    </AuthenticatedLayout>
+</template>
+
+<script setup>
+    import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+    import InputError from '@/Components/InputError.vue';
+    import PrimaryButton from '@/Components/PrimaryButton.vue';
+    import { useForm, Head } from '@inertiajs/vue3';
+    
+    const form = useForm({
+        name: '',
+    });
+</script>
