702d64fc8324775dfed434b30be534ff2cdf21c2
Lukeylas <120398798+Lukeylas@users.noreply.github.com>
2024-10-13 18:36:29 +0200

    Updated Default Schema Character Limit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
@@ -21,5 +22,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
+        Schema::defaultStringLength(191);
     }
 }
