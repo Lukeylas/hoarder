2e349551578eb893ff3cdcd64a2ab55835ec1334
Lukeylas <120398798+Lukeylas@users.noreply.github.com>
2024-10-13 18:36:59 +0200

    Added User Relation to Item model

--- database/migrations/2024_10_13_142726_create_items_table.php
+++ database/migrations/2024_10_13_142726_create_items_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('items', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('name');
             $table->timestamps();
         });
     }
